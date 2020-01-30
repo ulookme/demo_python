98bff9b45f4bc9514cc7d1ca675cec9efc10919b
charles <charles.hajjar01@gmail.com>
2020-01-30 15:12:55 +0100

    teste avec programme de replacement de classe random

---
+++ roulement.py
@@ -0,0 +1,21 @@
+import random
+
+classe = ["Fakhredine","Clément" ,"Atilla","Jean-Guillaume",
+"Mickael", "Nicolas D", "Vincent","Reem",
+"Charles", "Karima", "Jacques","Thibaut",
+"Nicolas P", "Quentin", "Adeline", "Pierre M",
+"Dino", "Simon", "Damien", "Jason", 
+"Samuel", "Colombe", "Emilie","Pierre E"]
+print(classe)
+
+Table1=[]
+table2=[]
+table3=[] 
+table4=[] 
+table5=[] 
+table6=[]
+print(Table1)
+
+
+
+
